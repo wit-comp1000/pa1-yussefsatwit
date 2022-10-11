caad12c2dddd46edc273b993b3a55ec64cc4dbbd
yussefs <yussefs@WIT45003183.wit.private>
2022-10-11 10:48:33 -0400

    PA1 with comments

--- comp1000_pa1/src/comp1000_pa1/PA1a.java
+++ comp1000_pa1/src/comp1000_pa1/PA1a.java
@@ -1,5 +1,39 @@
 package comp1000_pa1;
 
+import java.util.Scanner;
+
+/**
+ * Solution to the first part
+ * 
+ * @author Sarah Yussef 
+ */
+import.java.util.Scanner;
+
 public class PA1a {
+	
+	/** 
+	 * Starts the program 
+	 * 
+	 * @param args convert yards, feet and inches to a total number of inches 
+	 */
+	
+	public static void main(String [] args) {
+		//asks for input from the user
+		Scanner in = new Scanner(System.in); 
+		//insert number of yards
+		System.out.print("Enter number of yards: ");
+		int yards = in.nextInt();
+		//insert number of feet
+		System.out.print("Enter number of feet: ");
+		int feet = in.nextInt();
+		//insert number of inches
+		System.out.print("Enter number of inches: ");
+		int inches = in.nextInt();
+		//calculate total in inches 
+		int totalInches = (((yards * 3) + feet) * 12) + inches;
+		//prints total in inches 
+		System.out.println("Total number of inches: " + totalInches);
+		
+	}
 
 }
--- comp1000_pa1/src/comp1000_pa1/PA1b.java
+++ comp1000_pa1/src/comp1000_pa1/PA1b.java
@@ -1,8 +1,40 @@
 package comp1000_pa1;
 
+import java.util.Scanner;
+
+/**
+ * Solution to the second part
+ * 
+ * @author Sarah Yussef 
+ */
+
 public class PA1b {
+	
+	/** 
+	 * Starts the program in "reverse"
+	 * 
+	 * @param args convert a number of inches into yards, feet and inches
+	 */
+	
 	public static void main(String[] args) {
-		// your code here
+		//asks for input from the user 
+				Scanner in = new Scanner(System.in);
+				//prints question
+				System.out.print("Enter number of inches: ");
+				//asks for number of inches 
+				int inches = in.nextInt();
+				//converts the number of inches into yards
+				int yards = inches / 36;
+				//one yard is 3feet=36inches so divide inches by 36 to find value in yards ^
+				inches %= 36;
+				// use the remainder from the inches
+				int feet = inches / 12;
+				// one feet is 12 inches so divide
+				inches %= 12;
+				//prints number of yards, feet and inches respectively 
+				System.out.printf("Yards: %d%n", yards);
+				System.out.printf("Feet: %d%n", feet);
+				System.out.printf("Inches: %d%n", inches);
 	}
 
 }
